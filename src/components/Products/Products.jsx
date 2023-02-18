33d064ead6c105ba1b82045ebb1b8e4067479cbe
Vaughn Pole <vjp2109@columbia.edu>
2023-02-18 00:13:50 -0500

    Set up ecommerce site with two paintings so far

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import okamura1 from "../../images/okamura-1.jpeg";
+import okamura2 from "../../images/okamura-2.jpeg";
+import okamura3 from "../../images/okamura-3.jpeg";
+import okamura4 from "../../images/okamura-4.jpeg";
+import okamura5 from "../../images/okamura-5.jpeg";
+
+import Product from "../Product/Product";
+
+const products = [
+  {
+    id: 1,
+    name: "Nurse Tracey",
+    descripiton: "Oil on linen 60 X 40 in.",
+    price: "$300.00",
+    image: okamura1,
+  },
+  {
+    id: 2,
+    name: "Brooklyn Myth...",
+    descripiton: "Oil, paint marker on canvas 64 x 64 in.",
+    price: "$450.00",
+    image: okamura2,
+  },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
