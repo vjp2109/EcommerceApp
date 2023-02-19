a8450d78c486fb90a9bdaaa36dfbb42cc2f57804
Vaughn Pole <vjp2109@columbia.edu>
2023-02-18 19:03:15 -0500

    deleted painting images, implemented commercejs, got everything to render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+// This does a lot of the heavy lifting for the backend work - commercejs is great :)
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    // we have to wait to see what inside the promise
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import okamura1 from "../../images/okamura-1.jpeg";
-import okamura2 from "../../images/okamura-2.jpeg";
-import okamura3 from "../../images/okamura-3.jpeg";
-import okamura4 from "../../images/okamura-4.jpeg";
-import okamura5 from "../../images/okamura-5.jpeg";
 
 import Product from "../Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Nurse Tracey",
-    descripiton: "Oil on linen 60 X 40 in.",
-    price: "$300.00",
-    image: okamura1,
-  },
-  {
-    id: 2,
-    name: "Brooklyn Myth...",
-    descripiton: "Oil, paint marker on canvas 64 x 64 in.",
-    price: "$450.00",
-    image: okamura2,
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
